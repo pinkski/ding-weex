14d907bf17f702173fe31e4dab24a744a764f6d8
lynn.zhu <zxl20zxl@gmail.com>
2017-02-10 10:01:22 +0800

    add tabbar

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,29 @@ require('webpack')
 require('weex-loader')
 
 var path = require('path')
+var fs = require('fs')
 
+var entry = {};
+function walk(dir, root) {
+  var directory = path.join(__dirname, root, dir)
+  fs.readdirSync(directory)
+      .forEach(function (file) {
+        var fullpath = path.join(directory, file)
+        var stat = fs.statSync(fullpath)
+
+        if (stat.isFile() &&
+            path.extname(fullpath) === '.we') {
+          var name = path.join(dir, path.basename(file, '.we'))
+          entry[name] = fullpath + '?entry=true'
+        } else if (stat.isDirectory()) {
+          var subdir = path.join(dir, file)
+          walk(subdir, root)
+        }
+      })
+}
+walk('./', 'src');
 module.exports = {
-  entry: {
-    main: path.join(__dirname, 'src', 'weex-bootstrap.we?entry=true')
-  },
+  entry: entry,
   output: {
     path: 'dist',
     filename: '[name].js'
@@ -19,4 +37,4 @@ module.exports = {
       }
     ]
   }
-}
+};
